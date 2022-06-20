14c123c243807f44459057387b3e522fc5b7440c
harkeju <61701308+harkeju@users.noreply.github.com>
2022-06-20 20:46:48 +0100

    Renamed the project

--- akeju/akeju/urls.py
+++ akeju/akeju/urls.py
@@ -1,4 +1,4 @@
-"""I4G011721OZX URL Configuration
+"""akeju URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
